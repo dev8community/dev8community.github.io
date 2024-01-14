931412db64192fb163962cd2d5d7451b3785acc1
Sean Francis N. Ballais <sean@seanballais.com>
2024-01-14 18:31:46 +0800

    Add support for building files and serving them locally.

--- tool.py
+++ tool.py
@@ -1,4 +1,14 @@
 import argparse
+import functools
+import http.server
+import logging
+import os
+import socketserver
+import shutil
+
+from pathlib import Path
+
+import toolconfig
 
 
 def _create_parser() -> argparse.ArgumentParser:
@@ -10,15 +20,39 @@ def _create_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def _build():
-    pass
+def _build(base_dist_dir=Path('dist')):
+    os.makedirs(base_dist_dir, exist_ok=True)
+
+    for route, html_file in toolconfig.page_routes.items():
+        route: str = route.strip('/')  # Having '/' at the start messes up Path.
+
+        src: Path = html_file
+        dest: Path = base_dist_dir / route / 'index.html'
+        
+        dest.parent.mkdir(exist_ok=True, parents=True)
+
+        shutil.copy(src, dest)
 
 
-def _serve():
-    pass
+def _serve(base_dist_dir=Path('dist')):
+    _build(base_dist_dir)
+
+    # Set up basic server.
+    handler = functools.partial(http.server.SimpleHTTPRequestHandler,
+                                directory=base_dist_dir)
+    httpd: socketserver.TCPServer = socketserver.TCPServer(('localhost', 2016),
+                                                           handler)
+    httpd.allow_reuse_address = True
+    
+    httpd.serve_forever()
+    httpd.server_close()
 
 
 if __name__ == '__main__':
     parser: argparse.ArgumentParser = _create_parser()
-    parser.parse_args()
-    print(parser)
+    args: argparse.Namespace = parser.parse_args()
+    
+    if args.task == 'build':
+        _build()
+    elif args.task == 'serve':
+        _serve()
--- toolconfig.py
+++ toolconfig.py
@@ -2,6 +2,7 @@ output = {
     
 }
 
+# Paths are relative to tool.py.
 page_routes = {
     '/': 'pages/index.html',
     '/surveys/evtss2023/': 'pages/surveys/evtss2023.html'
