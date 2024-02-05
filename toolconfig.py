9e3b8e73264c0873d2bffbaac4194f0628aa752a
Sean Francis N. Ballais <sean@seanballais.com>
2024-02-05 22:59:09 +0800

    Finish the 'Location' section.

--- toolconfig.py
+++ toolconfig.py
@@ -19,7 +19,8 @@ data = {
     'length_of_professional_programming_experience': 'data/surveys/evtss2023/length-of-professional-programming-experience.json',
     'length_of_programming_experience_by_developer_type': 'data/surveys/evtss2023/length-of-programming-experience-by-developer-type.json',
     'length_of_professional_programming_experience_by_developer_type': 'data/surveys/evtss2023/length-of-professional-programming-experience-by-developer-type.json',
-    'developer_activities': 'data/surveys/evtss2023/developer-activities.json'
+    'developer_activities': 'data/surveys/evtss2023/developer-activities.json',
+    'location': 'data/surveys/evtss2023/location.json'
 }
 
 # Routes that we can access in the URL. Paths are
