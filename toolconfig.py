f216d27b9f716e42c728aa6dfe93acdb9a91b2d8
Sean Francis N. Ballais <sean@seanballais.com>
2024-02-07 23:32:20 +0800

    Finish the 'Event Food and Drinks' section.

--- toolconfig.py
+++ toolconfig.py
@@ -50,7 +50,8 @@ data = {
     'desired_tech_events_in_the_region': 'data/surveys/evtss2023/desired-tech-events-in-the-region.json',
     'desired_tech_event_topics': 'data/surveys/evtss2023/desired-tech-event-topics.json',
     'survey_length': 'data/surveys/evtss2023/survey-length.json',
-    'survey_difficulty': 'data/surveys/evtss2023/survey-difficulty.json'
+    'survey_difficulty': 'data/surveys/evtss2023/survey-difficulty.json',
+    'event_food_and_drinks': 'data/surveys/evtss2023/event-food-and-drinks.json'
 }
 
 # Routes that we can access in the URL. Paths are
