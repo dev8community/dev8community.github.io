565e4f5c6c9a4f352938bac0d044729cdc53d83a
Sean Francis N. Ballais <sean@seanballais.com>
2024-02-07 23:04:27 +0800

    Finish the 'Survey Difficulty' section.

--- toolconfig.py
+++ toolconfig.py
@@ -49,7 +49,8 @@ data = {
     'local_cs_it_sentiments': 'data/surveys/evtss2023/local-cs-it-sentiments.json',
     'desired_tech_events_in_the_region': 'data/surveys/evtss2023/desired-tech-events-in-the-region.json',
     'desired_tech_event_topics': 'data/surveys/evtss2023/desired-tech-event-topics.json',
-    'survey_length': 'data/surveys/evtss2023/survey-length.json'
+    'survey_length': 'data/surveys/evtss2023/survey-length.json',
+    'survey_difficulty': 'data/surveys/evtss2023/survey-difficulty.json'
 }
 
 # Routes that we can access in the URL. Paths are
