106928ece44fa3671fd3fe1b4c1ed26c7186b4d3
FlamingHerb <mbomisol@up.edu.ph>
2025-10-27 00:50:58 +0800

    Add COMPILE 2025 web page.

---
+++ assets/events/compile/2025/js/index.js
@@ -0,0 +1,19 @@
+﻿function onDocumentReady()
+{
+    loopLogo();
+}
+
+function loopLogo()
+{
+    let logoElem = document.getElementById('logo');
+    let frameNum = 0;
+    setInterval(() => {
+        logoElem.src = '/assets/events/compile/2025'
+            + '/img/logo-frames/'
+            + `logo-frame-${frameNum % 2}.png`;
+
+        frameNum++;
+    }, 800);
+}
+
+document.addEventListener('DOMContentLoaded', onDocumentReady);
